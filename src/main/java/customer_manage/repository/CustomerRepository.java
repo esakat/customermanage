f88f0664759d66502cd23f13f0efc7ddc972b94b
tomoyaRed <esaka.tom@gmail.com>
2018-01-03 01:00:01 +0900

    miss

--- src/main/java/customer_manage/repository/CustomerRepository.java
+++ src/main/java/customer_manage/repository/CustomerRepository.java
@@ -6,9 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     @Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
-    List<Customer> findAllOrderByName(Pageable pageable);
+    Page<Customer> findAllOrderByName(Pageable pageable);
 }
