e8fd91526fca2126caec7c14059cd95054f6db1b
tomoyaRed <esaka.tom@gmail.com>
2018-01-02 13:43:27 +0900

    complete 2.2

--- src/main/java/customer_manage/App.java
+++ src/main/java/customer_manage/App.java
@@ -1,13 +1,21 @@
 package customer_manage;
 
+import customer_manage.domain.Customer;
+import customer_manage.repository.CustomerRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by tom_red on 2018/01/02.
@@ -16,15 +24,17 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 @ComponentScan
 public class App implements CommandLineRunner {
     @Autowired
-    NamedParameterJdbcTemplate jdbcTemplate;
+    CustomerRepository customerRepository;
 
     @Override
     public void run(String... strings) throws Exception {
-        String sql = "SELECT 1";
-        SqlParameterSource param = new MapSqlParameterSource();
-        Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+        // データ追加
+        Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
+        System.out.println(created + " is created!");
 
-        System.out.println("result = " + result);
+        // データ表示
+        customerRepository.findAll()
+                .forEach(System.out::println);
     }
 
     public static void main(String[] args) {
